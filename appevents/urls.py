a947a50ff8fcdda102dbe77b77e223d2dc824a96
lenwe171175 <robin.sylvestre@live.fr>
2022-06-23 16:00:39 +0200

    export to xls

--- appevents/urls.py
+++ appevents/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path("subevent/<params>/<step>", views.subevent, name="subevent"),
     path("subproductevent/<step>/<product_to_sub>", views.subproductevent, name="subproductevent"),
     path("exportparticipationincsv/<event>", views.exportparticipationincsv, name="exportparticipationincsv"),
+    path("exportparticipationinxls/<event>", views.exportparticipationinxls, name="exportparticipationinxls"),
     path("listeventstobucque", views.listeventstobucque, name="listeventstobucque"),
     path("eventtobucque/<event>", views.eventtobucque, name="eventtobucque")
 ]
