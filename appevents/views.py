0787d24000d1539171e3ee215622b68a8d248f50
lenwe171175 <robin.sylvestre@live.fr>
2022-06-23 12:24:40 +0200

    Bucquage event system

--- appevents/views.py
+++ appevents/views.py
@@ -118,10 +118,9 @@ def eventtobucque(request, event):
 def manageparticipationfile(file,event):
     error=0
     file_data = file.read().decode("utf-8-sig")
-    rows = file_data.split("'")
+    rows = file_data.split("\r\n")
     for row in rows: #pour chaque ligne du fichier
-        row2=row.split(";")
-        row2.patate
+        row=row.split(";")
         if row[0] != 'ID Participation': #on saute la première ligne de headers
             if Participation_event.objects.filter(pk=row[0]).count()==1: #si la participation existe
                 targetparticipation=Participation_event.objects.get(pk=row[0])
