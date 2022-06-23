8abecddb2fba7bedea430d4249fa0a1bacb08f24
lenwe171175 <robin.sylvestre@live.fr>
2022-06-23 16:58:45 +0200

    import participation from xls

--- appevents/models.py
+++ appevents/models.py
@@ -1,5 +1,4 @@
 from decimal import Decimal
-from unicodedata import decimal
 from django.db import models
 from appkfet.models import Consommateur, History
 from django.contrib.auth.models import User
@@ -42,7 +41,7 @@ class Participation_event(models.Model):
 
     def save(self, *args, **kwargs):
         if self.participation_ok==True and self.participation_bucquee==False:
-            prix_total=decimal.Decimal(self.number) * self.product_participation.prix
+            prix_total=Decimal(self.number) * self.product_participation.prix
             if Consommateur.testdebit(self.cible_participation, prix_total):
                 self.participation_bucquee=True
                 super(Participation_event, self).save(*args, **kwargs)
