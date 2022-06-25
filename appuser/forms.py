4a7d655117db56e894e201e3d860a9cddee0ac7d
lenwe171175 <robin.sylvestre@live.fr>
2022-06-25 10:47:35 +0200

    Adding captcha to inscription form

--- appuser/forms.py
+++ appuser/forms.py
@@ -21,7 +21,7 @@ class inscriptionform(forms.ModelForm):
     )
     captcha = CaptchaField(
         required=True,
-        widget=forms.CustomCaptchaTextInput(
+        widget=forms.CaptchaTextInput(
             attrs={"placeholder": "Captcha"}
         ),
     )
