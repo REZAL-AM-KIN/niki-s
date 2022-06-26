57d1d8b068be7b52b160ae13a162fceec248ddad
lenwe171175 <robin.sylvestre@live.fr>
2022-06-26 10:13:59 +0200

    Adding pagination on api

--- niki/settings.py
+++ niki/settings.py
@@ -141,6 +141,11 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    "PAGE_SIZE": 25,
+}
+
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
