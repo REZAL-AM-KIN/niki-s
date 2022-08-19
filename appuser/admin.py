ee5892698ef4adc4baef61704177815b69a9894b
Rhadamanthe76 <107307293+Teiwin@users.noreply.github.com>
2022-08-19 14:57:36 +0200

    added has_cotiz to admin list_field

--- appuser/admin.py
+++ appuser/admin.py
@@ -2,8 +2,17 @@ from django.contrib import admin
 from django.contrib.auth.models import User, Group
 from .models import Utilisateur, Groupe
 
+from datetime import date
+
 
 class AdminUtilisateur(admin.ModelAdmin):
+    def has_cotiz(adminUser, user):
+        current_date = date.today()
+        return current_date <= user.date_expiration
+
+    # to display the tick and not "True"
+    has_cotiz.boolean = True
+
     list_display = (
         "username",
         "first_name",
@@ -12,6 +21,7 @@ class AdminUtilisateur(admin.ModelAdmin):
         "email",
         "chambre",
         "is_active",
+        "has_cotiz",
         "is_gadz",
         "is_conscrit",
         "is_staff",
