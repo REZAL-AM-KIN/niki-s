32d0bea31b43da6056fa857ab2e9ab4770ffbba0
Rhadamanthe76 <107307293+Teiwin@users.noreply.github.com>
2022-09-07 13:59:28 +0200

    set jwt auth first

--- niki/settings.py
+++ niki/settings.py
@@ -160,8 +160,8 @@ AUTH_PASSWORD_VALIDATORS = [
 
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
-        "rest_framework.authentication.SessionAuthentication",
         "rest_framework_simplejwt.authentication.JWTAuthentication",
+        "rest_framework.authentication.SessionAuthentication",
     ],
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
     "PAGE_SIZE": 25,
