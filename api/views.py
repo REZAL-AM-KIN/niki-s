55161fc902280f967503cc9c20d93e5153e7d778
OSiG43 <simon.galand43@gmail.com>
2023-02-26 01:50:36 +0100

    Supression de la permission de pian'ss pour acceder à ses permissions (on doit toujours pouvoir acceder à l'endpoint qui donne la liste des permissions que l'on a)
    EDIT: Ajout du fichier oublié

--- api/views.py
+++ api/views.py
@@ -19,7 +19,7 @@ from api.permissions import AllowedPianss, AllowedIPEvenSaveMethods, get_client_
 class PermissionsViewSet(viewsets.ModelViewSet):
     serializer_class = PermissionsSerializer
     http_method_names = ["get", "options"]
-    permission_classes = (permissions.DjangoModelPermissions, AllowedIPEvenSaveMethods,)
+    permission_classes = (permissions.DjangoModelPermissions,)
     queryset = Utilisateur.objects.none()
 
     def list(self, request):
